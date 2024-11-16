46cc610735f6c0b0d151e9d88459c8fc81be757a
Siri Chandra <siri@Siris-MacBook-Pro-3.local>
2024-11-16 16:30:42 -0600

    Added PySimpleGUI app

--- python_version/app.py
+++ python_version/app.py
@@ -2,17 +2,38 @@
 
 import PySimpleGUI as sg
 
-layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
+layout = [
+    [sg.Text("Hello! I'm your Skincare Helper!", font=('Poppins', 20))],
+    [sg.Text("Let's find the perfect product for you", font=('Poppins', 15))],
+
+    [sg.Text("What is your skin type?", font=('Poppins', 12))],
+    [sg.Combo(['Oily', 'Dry', 'Combination', 'Sensitive'], key='-SKIN_TYPE-', font=('Poppins', 12))],
+    
+    [sg.Text("What is your main skincare concern?", font=('Poppins', 12))],
+    [sg.Combo(['Acne', 'Wrinkles', 'Hyperpigmentation', 'Dryness'], key='-CONCERN-', font=('Poppins', 12))],
+    
+    [sg.Text("What's your budget?", font=('Poppins', 12))],
+    [sg.Combo(['Under $20', '$20 - $50', '$50 - $100', 'Above $100'], key='-BUDGET-', font=('Poppins', 12))],
+    
+    [sg.Button('Get Recommendations', font=('Poppins', 12), size=(20, 2))]
+]
 
 # Create the window
-window = sg.Window("Demo", layout)
+window = sg.Window('Skincare Helper', layout, size=(400,400))
 
 # Create an event loop
 while True:
     event, values = window.read()
-    # End program if user closes window or
-    # presses the OK button
-    if event == "OK" or event == sg.WIN_CLOSED:
+
+    if event == sg.WINDOW_CLOSED:
         break
+
+    if event == 'Get Recommendations':
+        skin_type = values['-SKIN_TYPE-']
+        concern = values['-CONCERN-']
+        budget = values['-BUDGET-']
+        
+        sg.popup(f"Skin Type: {skin_type}\nConcern: {concern}\nBudget: {budget}")
+
 
 window.close()
