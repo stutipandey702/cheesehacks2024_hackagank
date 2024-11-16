feb8556ed6e97c1a558ab7684b63c0f1a2b497f0
Siri Chandra <siri@Siris-MacBook-Pro-3.local>
2024-11-16 16:54:06 -0600

    changed background color

--- python_version/app.py
+++ python_version/app.py
@@ -3,23 +3,23 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Text("Hello! I'm your Skincare Helper!", font=('Poppins', 20))],
-    [sg.Text("Let's find the perfect product for you", font=('Poppins', 15))],
+    [sg.Text("Hello! I'm your Skincare Helper!", font=('Poppins', 20), text_color='white', background_color='#D1A1AF')],
+    [sg.Text("Let's find the perfect product for you", font=('Poppins', 15), text_color='white', background_color='#D1A1AF')],
 
-    [sg.Text("What is your skin type?", font=('Poppins', 12))],
-    [sg.Combo(['Oily', 'Dry', 'Combination', 'Sensitive'], key='-SKIN_TYPE-', font=('Poppins', 12))],
+    [sg.Text("What is your skin type?", font=('Poppins', 12), text_color='white', background_color='#D1A1AF')],
+    [sg.Combo(['Oily', 'Dry', 'Combination', 'Sensitive'], key='-SKIN_TYPE-', font=('Poppins', 12), background_color='#D1A1AF', text_color = 'white')],
     
-    [sg.Text("What is your main skincare concern?", font=('Poppins', 12))],
-    [sg.Combo(['Acne', 'Wrinkles', 'Hyperpigmentation', 'Dryness'], key='-CONCERN-', font=('Poppins', 12))],
+    [sg.Text("What is your main skincare concern?", font=('Poppins', 12), text_color='white', background_color='#D1A1AF')],
+    [sg.Combo(['Acne', 'Wrinkles', 'Hyperpigmentation', 'Dryness'], key='-CONCERN-', font=('Poppins', 12), background_color='#D1A1AF', text_color='white')],
     
-    [sg.Text("What's your budget?", font=('Poppins', 12))],
-    [sg.Combo(['Under $20', '$20 - $50', '$50 - $100', 'Above $100'], key='-BUDGET-', font=('Poppins', 12))],
+    [sg.Text("What's your budget?", font=('Poppins', 12), text_color='white', background_color='#D1A1AF')],
+    [sg.Combo(['Under $20', '$20 - $50', '$50 - $100', 'Above $100'], key='-BUDGET-', font=('Poppins', 12), background_color='#D1A1AF', text_color='white')],
     
-    [sg.Button('Get Recommendations', font=('Poppins', 12), size=(20, 2))]
+    [sg.Button('Get Recommendations', font=('Poppins', 12), size=(20, 2), button_color=('white', '#D1A1AF'))]
 ]
 
 # Create the window
-window = sg.Window('Skincare Helper', layout, size=(400,400))
+window = sg.Window('Skincare Helper', layout, size=(400,400), background_color='#D1A1AF')
 
 # Create an event loop
 while True:
